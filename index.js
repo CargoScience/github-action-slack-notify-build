9370891410febbeaf3202973e9800ee63baafb3d
Paul Bromwell <paul@itechpros.com>
2024-07-23 16:38:45 -0400

    Include the author and the commit information.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const { buildSlackAttachments, formatChannelName } = require('./src/utils');
     const status = core.getInput('status');
     const color = core.getInput('color');
     const messageId = core.getInput('message_id');
+    const author = core.getInput('author');
+    const commit_message = core.getInput('commit_message');
     const token = process.env.SLACK_BOT_TOKEN;
     const slack = new WebClient(token);
 
@@ -17,7 +19,7 @@ const { buildSlackAttachments, formatChannelName } = require('./src/utils');
       return;
     }
 
-    const attachments = buildSlackAttachments({ status, color, github });
+    const attachments = buildSlackAttachments({ status, color, github, author, commit_message });
     const channelId = core.getInput('channel_id') || (await lookUpChannelId({ slack, channel }));
 
     if (!channelId) {
